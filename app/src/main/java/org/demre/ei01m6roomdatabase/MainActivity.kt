9584425eccc5878f3a80be9e71d2b50014317e67
hipolito <huaylla7@hotmail.com>
2023-07-25 22:15:32 -0400

    Creación del proyecto

---
+++ app/src/main/java/org/demre/ei01m6roomdatabase/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.demre.ei01m6roomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

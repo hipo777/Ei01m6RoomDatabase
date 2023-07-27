8424ff1bd812e4d3e9b4bb3a6da3b880d3f5f626
hipolito <huaylla7@hotmail.com>
2023-07-26 20:22:23 -0400

    Fragmentos creados

--- app/src/main/java/org/demre/ei01m6roomdatabase/MainActivity.kt
+++ app/src/main/java/org/demre/ei01m6roomdatabase/MainActivity.kt
@@ -2,10 +2,28 @@ package org.demre.ei01m6roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.demre.ei01m6roomdatabase.databinding.ActivityMainBinding
+/*
+    [] Navigation
+    [] Fragmento para agregar
+    [] Crear Interfaz
+    [] Room
+        [] Dao
+        [] BaseDatos
+        [] Entity
+    [] RecyclerView
+    [] Fragmento para listado
 
+
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //private lateinit var appDb : AppDatabase
+    //private lateinit var userAdapter: UserAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

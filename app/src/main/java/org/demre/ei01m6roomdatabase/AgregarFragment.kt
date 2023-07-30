9bea1575c25f04c946a5a9de42709af596795c5c
hipolito <huaylla7@hotmail.com>
2023-07-29 22:28:03 -0400

    Con ViewModel listo Individual 03

--- app/src/main/java/org/demre/ei01m6roomdatabase/AgregarFragment.kt
+++ app/src/main/java/org/demre/ei01m6roomdatabase/AgregarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.demre.ei01m6roomdatabase.databinding.FragmentAgregarBinding
@@ -12,37 +13,37 @@ import org.demre.ei01m6roomdatabase.databinding.FragmentAgregarBinding
 class AgregarFragment : Fragment() {
 
     lateinit var binding: FragmentAgregarBinding
-    lateinit var repositorio: Repositorio
+    private val tareaVM: TareaVM by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentAgregarBinding.inflate(layoutInflater, container, false)
-        initRepositorio()
+
         initListener()
         recuperarTareas()
         return binding.root
     }
 
-    private fun initRepositorio() {
-        repositorio = Repositorio(TareaBaseDatos.getDatabase(requireContext() ).getTaskDao())
-    }
 
     private fun initListener() {
-        binding.btnIngresar.setOnClickListener(){
-            val texto =binding.etIngreso.text.toString()
+        binding.btnIngresar.setOnClickListener() {
+            val texto = binding.etIngreso.text.toString()
             guardarTarea(texto)
         }
     }
+
     private fun guardarTarea(texto: String) {
 
         val tarea = Tarea(texto)
-        GlobalScope.launch { repositorio.insertTask(tarea) }
-    }
-    private fun recuperarTareas(){
+        tareaVM.insertarTareas(tarea)
 
-        repositorio.getTareas().observe(requireActivity()){
+    }
+
+    private fun recuperarTareas() {
+
+        tareaVM.obtenerTareas().observe(viewLifecycleOwner) {
             val tasksAsText = it.joinToString("\n") { it.nombre }
             binding.tvRecuperado.text = tasksAsText
         }
